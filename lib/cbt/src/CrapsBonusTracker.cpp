986e37a9d488b695d60c74e3f35185ed07f64772
curtisburke-eng <curtisburke.eng@gmail.com>
2026-03-17 21:30:40 -0600

    Create the backend

--- lib/cbt/src/CrapsBonusTracker.cpp
+++ lib/cbt/src/CrapsBonusTracker.cpp
@@ -12,17 +12,40 @@ using enum CbtErrorCodes;
 /**
  * @brief Constructor for CrapsBonusTracker. Initializes members
  */
-CrapsBonusTracker::CrapsBonusTracker()
+CrapsBonusTracker::CrapsBonusTracker() :
+    m_rollMap({
+        {2, false}, {3, false}, {4, false}, {5, false}, {6, false},
+        {8, false}, {9, false}, {10, false}, {11, false}, {12, false}
+    })
 {
 
 }
 
 
-/**
- * @brief Destructor.
- */
-CrapsBonusTracker::~CrapsBonusTracker()
+std::error_code CrapsBonusTracker::MarkNumber(uint8_t CurrentRoll)
 {
+    if(CurrentRoll < 2 || CurrentRoll > 12)
+    {
+        SPDLOG_ERROR("Roll must be between 2 and 12");
+        return eInvalidArgument;
+    }
+    
+    if(CurrentRoll == 7)
+    {
+        Reset();
+    }
+    else if (m_rollMap.at(CurrentRoll) == false) {
+        m_rollMap.at(CurrentRoll) = true;
+    }
 
+    return eOk;
+}
+
+
+void CrapsBonusTracker::Reset()
+{
+    for (auto& pair : m_rollMap) {
+        pair.second = false;
+    }
 }
 
--- lib/cbt/src/CrapsBonusTracker.h
+++ lib/cbt/src/CrapsBonusTracker.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <map>
 #include <memory>
 #include <system_error>
 
@@ -8,8 +9,14 @@ class CrapsBonusTracker
 {
 public:
     CrapsBonusTracker();
-    ~CrapsBonusTracker();
+    ~CrapsBonusTracker() = default;
+
+    std::map<uint8_t, bool> GetRollMap() { return m_rollMap; };
+    std::error_code MarkNumber(uint8_t CurrentRoll);
+    
+    void Reset();
 
 private:
+    std::map<uint8_t, bool> m_rollMap;
     
 };
